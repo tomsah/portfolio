f4cca63c4a9ee98743cfb15ac130ad940e871c21
Thomas Salah <thomas.salah@gmail.com>
2019-07-30 09:09:35 +0100

    adding data and images

---
+++ src/js/data/Data.js
@@ -0,0 +1,98 @@
+const data = {
+  "projects": [
+    {
+      "index" : 0,
+      "id" : "project-1",
+      "name" : "Sakura",
+      "projectNumber" : "01",
+      "team" : "project",
+      "technologies" : [
+          "React",
+          "SASS",
+          "ES6",
+          "GSAP",
+          "node"
+      ],
+      "year" : "2018",
+      "image" : "./images/caseStudy/sakura/sakura-hero.png",
+      "image2" : "./images/caseStudy/sakura/sakura-mobile1.png",
+      "image3" : "./images/caseStudy/sakura/sakura-mobile2.png",
+      "image4" : "./images/caseStudy/sakura/sakura-homepage.png",
+      "statement" : "fullstack Javascript website for a restaurant with ediatbale menu",
+      "text": "Sakura is a beautiful design created by a talented designer. We are working in collaboration on personal side project to experiment and create beautiful things for web.The challenges here are to implement the design and create an admin section where menus and pictures are editable.I am building Sakura using a full javascript stack React and GSAP for the front-end, node with express and firebase for the backend.",
+      "statement2": "Side Project, Fullstack Javascript site using React, GSAP, Node, firebase"
+
+    },
+    {
+      "index" : 1,
+      "id" : "project-2",
+      "name" : "Bloodwise",
+      "projectNumber" : "02",
+      "team" : "Frontend",
+      "technologies" : [
+          "Drupal 7",
+          "SASS",
+          "JavaScript",
+          "HTML5",
+          "Foundation",
+          "RWD"
+      ],
+      "year" : "2013-16",
+      "image" : "./images/caseStudy/bloodwise/bloodwise-hero.png",
+      "image2" : "./images/caseStudy/bloodwise/bloodwise-mobile1.png",
+      "image3" : "./images/caseStudy/bloodwise/bloodwise-mobile2.png",
+      "image4" : "./images/caseStudy/bloodwise/bloodwise-homepage.png",
+      "statement" : "National charity, Helping to beat blood cancer",
+      "text": "As a frontend developer, at Bloodwise I first helped to maintain and improve an existing website. Following the company rebrand, we build a new site from scratch. I was responsible for the complete front-end rebuild of the site. This involved working with a custom Drupal theme and writing templates from scratch using SASS, Javascript and Zurb Foundation. The work involved lots of prototyping and ensuring the site is responsive using a mobile first approach. We distributed our code via github using continuous integration and New Relic to monitor the site.",
+      "statement2": "Drupal 7 fully responsive custom theme using foundation"
+    },
+    {
+      "index" : 2,
+      "id" : "project-3",
+      "name" : "Catch",
+      "projectNumber" : "03",
+      "team" : "Frontend",
+      "technologies" : [
+          "JavaScript",
+          "SASS",
+          "Drupal 8 & 7",
+          "HTML5",
+          "Bootstrap",
+          "RWD"
+      ],
+      "year" : "2016-17",
+      "image" : "./images/caseStudy/catch/catch.png",
+      "image2" : "./images/caseStudy/catch/catch-client-mobile.png",
+      "image3" : "./images/caseStudy/catch/cruk-pub-mobile.png",
+      "statement" : "London based International digitsal agency",
+      "text": "At Catch Digital, I was part of the frontend team. I worked on a large and complex Website for an international client using Drupal 8, Javascript, Sass and Twig. I also worked on client site for cancer research Uk, where I was involved in the build of a microsite using a inhouse pattern library, the creation of a custom drupal module to implement Olapic a 3rd party tool for social media sharing and helped for general maintenance. Working at Catch Digital has taught me the importance of working to tight deadlines, client communications and work efficiency.",
+      "statement2": "Frontend developer at catch working for national charity and international companies"
+    },
+    {
+      "index" : 3,
+      "id" : "project-4",
+      "name" : "SES",
+      "projectNumber" : "04",
+      "team" : "catch",
+      "technologies" : [
+          "Drupal8",
+          "SASS",
+          "JavaScript",
+          "Jquery",
+          "HTML5",
+          "Twig",
+      ],
+      "year" : "2017",
+      "image" : "./images/caseStudy/ses/ses-hero.png",
+      "image2" : "./images/caseStudy/ses/ses-mobile1.jpg",
+      "image3" : "./images/caseStudy/ses/ses-mobile2.jpg",
+      "image4" : "./images/caseStudy/ses/ses-homepage.png",
+      "statement" : "website build for satellites operator, ses",
+      "text": "I was frontend developer on this project. SES is a complex and large Drupal 8 website, fully responsive build with a mobile first approach. To deliver a scalable and comprehensive front-end system, we decided to create a static style-guide of individual components using BEM syntax and  an OOCSS approach.",
+      "statement2": "Fined tuned custom css/js Fully responsive. OOCSS approach. cross-browser "
+
+    },
+  ]
+}
+
+export default data;
